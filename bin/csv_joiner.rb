2fe986c9a8f65b6fc0b091faa09414e1bab2f2b4
Stelios Omirou <selemis@gmail.com>
2013-12-17 13:49:42 +0200

    Refactoring

--- bin/csv_joiner.rb
+++ bin/csv_joiner.rb
@@ -1,36 +1,25 @@
 class CsvJoiner
 
+  def common_lines(indexed_list1, indexed_list2)
+    indexed_list1.select{ |k,v| indexed_list2.include?(k)}.map{ |k,v| v}
+  end
+
+  def diff_lines(indexed_list1, indexed_list2)
+    indexed_list1.select{ |k,v| !indexed_list2.include?(k)}.map{ |k,v| v}
+  end
 
   private
 
-  # Compare 2 text files line by line.
-  # It returns new arrays.
-  # The first has the common lines.
-  # of the two files(comparing with the key).
-  # The second has the lines that are missing from file2lines.
-  def compare_indexed_lists(list1, list2)
-    common_lines = []
-    diff_lines = []
-    list1.each do |k, v|
-      unless list2.include?(k)
-        diff_lines << v
-      else
-        common_lines << v
-      end
-    end
-    return common_lines, diff_lines
-  end
-
   def extract_index(list, cols)
-    index = Hash.new
-    list.each do |line|
-      index[extract_key(line: line, cols: cols).join('~')] = line
+    index = list.map do |line|
+      [extract_key(line: line, cols: cols).join('~'), line]
     end
-    index
+    Hash[index]
   end
 
   def extract_key(options = {})
     data_split = options[:line].split(';')
+    data_split.find_all
     key = Array.new
     options[:cols].sort.each do |cn|
       key << data_split[cn-1]
--- spec/csv_joiner_spec.rb
+++ spec/csv_joiner_spec.rb
@@ -1,22 +1,25 @@
 require_relative '../bin/csv_joiner'
 
+def extract_index(list, cols)
+  @joiner.instance_eval do
+    extract_index(list, cols)
+  end
+end
+
 describe CsvJoiner do
 
   before do
-    @list = Array.new
-    @list << %w(1 2 3 4 5 6 7 8).join(';')
-    @list << %w(1 12 3 14 5 16 7 18).join(';')
+    @list = [%w(1 2 3 4 5 6 7 8).join(';'),
+             %w(1 12 3 14 5 16 7 18).join(';')]
 
-    @list1 = Array.new
-    @list1 << %w(1 2 3 4 5 6).join(';')
-    @list1 << %w(7 8 9 10 11 12).join(';')
-    @list1 << %w(13 14 15 16 17 18).join(';')
-    @list1 << %w(19 20 21 22 23 24).join(';')
+    @list1 = [%w(1 2 3 4 5 6).join(';'),
+              %w(7 8 9 10 11 12).join(';'),
+              %w(13 14 15 16 17 18).join(';'),
+              %w(19 20 21 22 23 24).join(';')]
 
-    @list2 = Array.new
-    @list2 << %w(1 3 5 7 9).join(';')
-    @list2 << %w(1 1 1 1 1 1).join(';')
-    @list2 << %w(13 15 2 2 2).join(';')
+    @list2 = [%w(1 3 5 7 9).join(';'),
+              %w(1 1 1 1 1 1).join(';'),
+              %w(13 15 2 2 2).join(';')]
 
     @joiner = CsvJoiner.new
   end
@@ -50,56 +53,31 @@ describe CsvJoiner do
 
   it 'compares two indexed lists based on the index and returns as common lines the whole line of the first list ' do
 
-    list1 = @list1
-    cols1 = [1, 3]
-    indexed_list1 = @joiner.instance_eval do
-      extract_index(list1, cols1)
-    end
+    @joiner.common_lines(
+        extract_index(@list1, [1, 3]),
+        extract_index(@list2, [1, 2])
+    ).should == [%w(1 2 3 4 5 6).join(';'),
+                 %w(13 14 15 16 17 18).join(';')]
 
-    list2 = @list2
-    cols2 = [1, 2]
-    indexed_list2 = @joiner.instance_eval do
-      extract_index(list2, cols2)
-    end
-
-    common1_actual = @joiner.instance_eval do
-      compare_indexed_lists(indexed_list1, indexed_list2)[0]
-    end
-    common1_expected = [%w(1 2 3 4 5 6).join(';'), %w(13 14 15 16 17 18).join(';')]
-    common1_actual.should == common1_expected
-
-    common2_actual = @joiner.instance_eval do
-      compare_indexed_lists(indexed_list2, indexed_list1)[0]
-    end
-    common2_expected = [%w(1 3 5 7 9).join(';'), %w(13 15 2 2 2).join(';')]
-    common2_actual.should == common2_expected
+    @joiner.common_lines(
+        extract_index(@list2, [1, 2]),
+        extract_index(@list1, [1, 3])
+    ).should == [%w(1 3 5 7 9).join(';'), %w(13 15 2 2 2).join(';')]
 
   end
 
   it 'compares two indexed lists based on the index and returns as different lines the whole line of the first list ' do
 
-    list1 = @list1
-    cols1 = [1, 3]
-    indexed_list1 = @joiner.instance_eval do
-      extract_index(list1, cols1)
-    end
+    @joiner.diff_lines(
+        extract_index(@list1, [1, 3]),
+        extract_index(@list2, [1, 2])
+    ).should == [%w(7 8 9 10 11 12).join(';'),
+                 %w(19 20 21 22 23 24).join(';')]
 
-    list2 = @list2
-    cols2 = [1, 2]
-    indexed_list2 = @joiner.instance_eval do
-      extract_index(list2, cols2)
-    end
-
-    @joiner.compare_indexed_lists(indexed_list1, indexed_list2)[1].should ==
-        [
-            %w(7 8 9 10 11 12).join(';'),
-            %w(19 20 21 22 23 24).join(';')
-        ]
-
-    @joiner.compare_indexed_lists(indexed_list2, indexed_list1)[1].should ==
-        [
-            %w(1 1 1 1 1 1).join(';')
-        ]
+    @joiner.diff_lines(
+        extract_index(@list2, [1, 2]),
+        extract_index(@list1, [1, 3])
+    ).should == [%w(1 1 1 1 1 1).join(';')]
 
   end
 
